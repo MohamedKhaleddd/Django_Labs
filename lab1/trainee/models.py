e46fdb5e530bd525625f5ddc92aed09b1b536d89
elftiany <mk5920201@gmail.com>
2026-05-08 23:57:48 +0300

    Lab2 Based on Lab1

--- lab1/trainee/models.py
+++ lab1/trainee/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Trainee(models.Model):
+    id= models.IntegerField(primary_key=True,verbose_name='ID',help_text='Trainee_ID')
+    name=models.CharField(max_length=100,verbose_name='Name',help_text='Trianee Name')
+    #test=models.TextField()
